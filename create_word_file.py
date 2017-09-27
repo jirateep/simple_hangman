ed04984ccd2ff1a1760393d8ab9e932866a38c44
jirateep <charoenshop@gmail.com>
2017-09-27 20:49:51 +0700

    delete symbols in word

--- create_word_file.py
+++ create_word_file.py
@@ -1,17 +1,23 @@
 #download dict here: http://www.bragitoff.com/2016/03/english-dictionary-in-csv-format/
 
+def is_included_symbols(word) :
+	for ch in word :
+		if not ord('a') <= ord(ch) <= ord('z') :
+			return True
+	return False
+
 fw = open('dict.txt','w')
 word_list = []
-for char in range(ord('A'),ord('Z') + 1) :
-	fr = open('Dictionary/' + chr(char) + '.csv', 'r')
+for ch in range(ord('A'),ord('Z') + 1) :
+	fr = open('Dictionary/' + chr(ch) + '.csv', 'r')
 	lines = fr.readlines()
 	for line in lines :
 		line = line.replace('\n','')
 		line = line[1:len(line)-1]
 		if len(line) > 0 :
 			cut_index = line.find(' ')
-			word = line[:cut_index]
-			if len(word) <= 1 :
+			word = line[:cut_index].lower()
+			if len(word) <= 1 or is_included_symbols(word) :
 				continue
 			description = line[cut_index+1:]
 			word_list.append({'word':word, 'description':[description]})
