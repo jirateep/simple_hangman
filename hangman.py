e9263c1a47c20a44145a46bdc6e8aa3cc1da0188
jirateep <charoenshop@gmail.com>
2017-09-27 21:40:27 +0700

    random word

--- hangman.py
+++ hangman.py
@@ -1,9 +1,12 @@
 import ast
 from pprint import pprint
+import random
 
 fr = open('dict.txt','r')
 line = fr.read()
 fr.close()
 word_list = ast.literal_eval(line)
+random_word = random.choice(list(word_list.keys()))
 
-pprint(word_list)
+print(random_word)
+#pprint(word_list)
