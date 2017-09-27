2fca40825de69cce9f44f36788073c812a893195
jirateep <charoenshop@gmail.com>
2017-09-27 22:51:07 +0700

    coloring

--- hangman.py
+++ hangman.py
@@ -1,8 +1,23 @@
 import ast
 from pprint import pprint
 import random
+import os
 
-def display_word(word,already_guess) :
+os.system('color 7')
+
+def color(word, color='') :
+	color = color.lower()
+	if color == 'blue' :
+		return f'\033[94m{word}\033[0m'
+	elif color == 'yellow' :
+		return f'\033[93m{word}\033[0m'
+	elif color == 'green' :
+		return f'\033[92m{word}\033[0m'
+	elif color == 'red' :
+		return f'\033[91m{word}\033[0m'
+	else :
+		return word
+def display_word(word, already_guess) :
 	print('GUESS THIS WORD: ',end='')
 	for ch in word :
 		if ch in already_guess :
@@ -26,19 +41,20 @@ def display_man(wrong) :
 		for i in tmp[1] :
 			if wrong <= 0 :
 				break
-			print(i,end='')
+			print(color(f'{i}','blue'),end='')
 			wrong -= 1
 		print()
 
 def print_end(correct,meaning) :
 	if correct==0 :
-		print('YOU WIN!!')
+		print(color('YOU WIN!!','green'))
 	else :
-		print('YOU LOSE!!')
+		print(color('YOU LOSE!!','red'))
 	print(f'word: {random_word}')
 	print('meaning:')
 	for description in meaning :
 		print(f'\t{description}')
+	print('_________________________________')
 
 fr = open('dict.txt','r')
 line = fr.read()
@@ -55,19 +71,19 @@ while wrong < 9 and correct > 0:
 	display_word(random_word,already_guess)
 	guess = str(input('guess character: ')).lower()
 	if len(guess) != 1 :
-		print('Try again, length != 1.')
+		print(color('Try again, length != 1.','yellow'))
 		continue
 	if not ord('a') <= ord(guess) <= ord('z') :
-		print('Try again, character including symbol.')
+		print(color('Try again, character including symbol.','yellow'))
 		continue
 	if guess in already_guess :
-		print('Try again, you already guess.')
+		print(color('Try again, you already guess.','yellow'))
 		continue
 	if guess in random_word :
-		print(f'Have letter {guess}.')
+		print(color(f'Have letter {guess}.','green'))
 		correct -= random_word.count(guess)
 	else :
-		print(f'Don\'t have letter {guess}.')
+		print(color(f'Don\'t have letter {guess}.','red'))
 		wrong += 1
 	already_guess.append(guess)
 print_end(correct, word_list[random_word])
