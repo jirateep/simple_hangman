3f4da661c3e4e863f6f2de814cd65a347cf1e8c2
jirateep <charoenshop@gmail.com>
2017-09-27 22:20:42 +0700

    add display_man

--- hangman.py
+++ hangman.py
@@ -11,6 +11,25 @@ def display_word(word,already_guess) :
 			print('-',end='')
 	print()
 
+def display_man(wrong) :
+	man = [[' _________',''],
+		   [' |        ','|'],
+		   [' |        ','O'],
+		   [' |        ','|'],
+		   [' |       ','/|\\'],
+		   [' |        ','|'],
+		   [' |       ','/\\'],
+		   ['=+====','']
+		  ]
+	for tmp in man :
+		print(tmp[0],end='')
+		for i in tmp[1] :
+			if wrong <= 0 :
+				break
+			print(i,end='')
+			wrong -= 1
+		print()
+
 fr = open('dict.txt','r')
 line = fr.read()
 fr.close()
@@ -19,7 +38,8 @@ random_word = random.choice(list(word_list.keys()))
 print(random_word)
 wrong = 0
 already_guess = []
-while wrong < 10 :
+while wrong < 9 :
+	display_man(wrong)
 	display_word(random_word,already_guess)
 	guess = str(input('guess character: ')).lower()
 	if len(guess) != 1 :
@@ -31,4 +51,9 @@ while wrong < 10 :
 	if guess in already_guess :
 		print('Try again, you already guess.')
 		continue
+	if guess in random_word :
+		print(f'Have letter {guess}.')
+	else :
+		print(f'Don\'t have letter {guess}.')
+		wrong += 1
 	already_guess.append(guess)
